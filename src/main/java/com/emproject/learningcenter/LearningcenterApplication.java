776040c8822f7c8fdb728aefa13de341beea5ba9
Emily <emily.zy.lin@gmail.com>
2024-07-15 21:53:37 -0500

    commit the backend setup

---
+++ src/main/java/com/emproject/learningcenter/LearningcenterApplication.java
@@ -0,0 +1,13 @@
+package com.emproject.learningcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningcenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emproject/learningcenter/LearningcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emproject.learningcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
