d5354e0e740fff7013b0d443c75614b82a430613
Emily <emily.zy.lin@gmail.com>
2024-08-06 21:02:48 -0500

    add route for Projects page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,34 @@
 import HomePage from "./layouts/HomePage/HomePage";
+import Projects from "./layouts/Projects/Projects";
 import Navbar from "./layouts/NavbarAndFooter/Navbar";
 import Footer from "./layouts/NavbarAndFooter/Footer";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import "./App.css"; //用于管理与 App 组件相关的样式
 
 function App() {
   return (
-    <div className="d-flex flex-column min-vh-100">
-      <Navbar />
-      <div className="flex-grow-1">
-        <HomePage />
+    <Router>
+      <div className="d-flex flex-column min-vh-100">
+        <Navbar />
+        <div className="flex-grow-1">
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="home" element={<HomePage />} />
+            {/* <Route path='/articles' element={<Articles />} /> */}
+            <Route path="projects" element={<Projects />} />
+            {/* <Route path='/leetcode' element={<Leetcode />} /> */}
+            {/* <Route path='/skills' element={<Skills />} /> */}
+            {/* <Route path='/contact' element={<ContactMe />} /> */}
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
---
+++ Frontend/src/layouts/Projects/Projects.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import DiariesList from "./DiariesList";
+
+function Projects() {
+  return (
+    <>
+      <h1>111</h1>
+      {/* <DiariesList /> */}
+    </>
+  );
+}
+
+export default Projects;
