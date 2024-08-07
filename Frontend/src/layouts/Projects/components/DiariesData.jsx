68afa2b724be827b39183b9e15bad50ffc09ef9c
Emily <emily.zy.lin@gmail.com>
2024-08-07 11:44:34 -0500

    each diary page is working

--- Frontend/src/layouts/Projects/components/DiariesData.jsx
+++ Frontend/src/layouts/Projects/components/DiariesData.jsx
@@ -3,13 +3,15 @@ const DiariesData = [
     id: 1,
     title: "First Blog Post",
     summary: "This is the summary of the first blog post.",
-    content: "This is the content of the first blog post.",
+    content:
+      "Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.",
   },
   {
     id: 2,
     title: "Second Blog Post",
     summary: "This is the summary of the second blog post.",
-    content: "This is the content of the second blog post.",
+    content:
+      "Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.",
   },
 ];
 
--- Frontend/src/layouts/Projects/components/ProjectDiary.jsx
+++ Frontend/src/layouts/Projects/components/ProjectDiary.jsx
@@ -15,10 +15,15 @@ function ProjectDiary() {
   return (
     <>
       <PageBanner title={diary.title} content={diary.summary} />
-      <div>
-        <h1>{diary.title}</h1>
-        <p>{diary.content}</p>
-      </div>
+      <article class="mb-4">
+        <div class="container px-4 px-lg-5">
+          <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-7">
+              <p>{diary.content}</p>
+            </div>
+          </div>
+        </div>
+      </article>
     </>
   );
 }
