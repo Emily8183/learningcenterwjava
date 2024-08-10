aa1d7cd2e426e6c42a3bf571b09c814065a4ccf6
Emily <emily.zy.lin@gmail.com>
2024-08-10 15:46:35 -0500

    create projectDiary class and repo

---
+++ Backend/spring-boot-library/src/main/java/com/emproject/learningcenterwithjava/dao/ProjectDiaryRepository.java
@@ -0,0 +1,9 @@
+package com.emproject.learningcenterwithjava.dao;
+
+import com.emproject.learningcenterwithjava.entity.ProjectDiary;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectDiaryRepository extends JpaRepository<ProjectDiary, Long> {
+
+}
+
--- Backend/spring-boot-library/src/main/java/com/emproject/learningcenterwithjava/entity/ProjectDiary.java
+++ Backend/spring-boot-library/src/main/java/com/emproject/learningcenterwithjava/entity/ProjectDiary.java
@@ -1,4 +1,15 @@
 package com.emproject.learningcenterwithjava.entity;
 
-public class ProjectDiary {
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="project_diaries")
+public class ProjectDiary extends Post{
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "projectName")
+    private String projectName;
+    
 }
