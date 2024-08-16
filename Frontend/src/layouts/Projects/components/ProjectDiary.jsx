ffcded7a129cc88829a476a62f002c1bc06fb2d4
Emily <emily.zy.lin@gmail.com>
2024-08-15 20:38:13 -0500

    set up the diary single page display

--- Frontend/src/layouts/Projects/components/ProjectDiary.jsx
+++ Frontend/src/layouts/Projects/components/ProjectDiary.jsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 import PageBanner from "../../NavbarAndFooter/PageBanner";
 // import DiariesData from "./DiariesData";
 import { useParams } from "react-router-dom";
-
+import axios from "axios";
 //如何在本页面陈列一个日记：
 // 1 从URL中提取字符；
-// 2 三个useState: setDiary, setLoading, setError
+// 2 三个useState: setProjectDiary, setLoading, setError
 // 3 用useEffect来fetchDiary
 // 4 if loading, log loading
 // 5 if error, log error
@@ -15,23 +15,34 @@ function ProjectDiary() {
   // const projectDiary = DiariesData.find((p) => p.id === parseInt(id));
   //当直接使用const data时，可以用以上代码，从 URL 参数中提取字符串，parseInt(id) 将字符串转换为整数
 
-  const [diary, setDiary] = useState(null);
+  const [projectDiary, setProjectDiary] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchSingleDiary = async () => {
       try {
-        const response = await axios.get(`/api/projectDiaries/${id}`);
-        setDiary(response.data);
+        const response = await axios.get(
+          `http://localhost:8080/api/projectDiaries/${id}`
+        );
+        setProjectDiary(response.data);
         setLoading(false);
       } catch (error) {
-        setError("error fetching diary");
+        setError("Error fetching diary");
         setLoading(false);
       }
     };
+
     fetchSingleDiary();
   }, [id]);
+
+  useEffect(() => {
+    console.log("loading:", loading);
+    console.log("error:", error);
+  }, [loading, error]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
 
   if (!projectDiary) {
     return <h2>Post not found</h2>;
@@ -40,16 +51,15 @@ function ProjectDiary() {
   return (
     <>
       <PageBanner title={projectDiary.title} content={projectDiary.summary} />
-      {/* <article className="mb-4">
+      <article className="mb-4">
         <div className="container px-4 px-lg-5">
           <div className="row gx-4 gx-lg-5 justify-content-center">
             <div className="col-md-10 col-lg-8 col-xl-7">
-              {/* TODO: content text size needs to change 
               <p>{projectDiary.content}</p>
             </div>
           </div>
         </div>
-      </article> */}
+      </article>
     </>
   );
 }
