adb58b1029f04cb94d297f58ed0d618ce62b0250
Emily <emily.zy.lin@gmail.com>
2024-08-25 17:05:38 -0500

    project list styling wip

--- Frontend/src/layouts/Projects/components/DiariesList.jsx
+++ Frontend/src/layouts/Projects/components/DiariesList.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-
+import { GoArrowUpRight } from "react-icons/go";
 // import DiariesData from "./DiariesData";
 
 function DiariesList() {
@@ -39,41 +39,47 @@ function DiariesList() {
 
   return (
     <>
-      <div className="container px-4 px-lg-5">
+      <section className="portfolio-section">
+        <div className="container">
+          {/* <div className="container px-4 px-lg-5">
         <div className="row gx-4 gx-lg-5 justify-content-center">
-          <div className="col-md-10 col-lg-8 col-xl-7">
-            <div className="post-preview">
-              {/* <a href="post.html">
-                <h2 className="post-title">Man must</h2>
-                <h3 className="post-subtitle">Problems</h3>
-              </a> */}
+          <div className="col-md-10 col-lg-8 col-xl-7"> */}
+          <div className="portfolio-box">
+            <div className="grid2">
               {diaries.map((projectDiary) => (
-                <div key={projectDiary.id}>
-                  <h2 className="post-title">
-                    <Link
-                      to={`/projectDiaries/${projectDiary.id}`}
-                      className="post-link"
-                    >
-                      {projectDiary.title}
-                    </Link>
-                  </h2>
-                  <h3 className="post-subtitle">
-                    <Link
-                      to={`/projectDiaries/${projectDiary.id}`}
-                      className="post-link"
-                    >
-                      {projectDiary.summary}
-                    </Link>
-                  </h3>
-                  <span className="post-meta">
-                    Posted on {projectDiary.date}
-                  </span>
+                <div className="portfolio-item branding" key={projectDiary.id}>
+                  <div className="image-box">
+                    <img src={`../../../leetcode/LC110.png`} alt="" />
+                  </div>
+
+                  <div className="content-box">
+                    <h3 className="portfolio-title">
+                      <Link
+                        to={`/projectDiaries/${projectDiary.id}`}
+                        className="post-link"
+                      >
+                        {projectDiary.title}
+                      </Link>
+                    </h3>
+
+                    <h4>
+                      <Link
+                        to={`/projectDiaries/${projectDiary.id}`}
+                        className="post-link"
+                      >
+                        {projectDiary.summary}
+                      </Link>
+                    </h4>
+                    <i>
+                      <GoArrowUpRight size={50} />
+                    </i>
+                  </div>
                 </div>
               ))}
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
