2206a07433d53d431c22cdc57e7ff74437b5c36e
Emily <emily.zy.lin@gmail.com>
2024-08-25 16:01:29 -0500

    update the layout of blog listing page

--- Frontend/src/layouts/Insights/components/BlogList.jsx
+++ Frontend/src/layouts/Insights/components/BlogList.jsx
@@ -5,32 +5,29 @@ import BlogData from "./BlogData";
 function BlogList() {
   return (
     <>
-      <header className="masthead">
-        <div
-          className="container position-relative px-4
-         px-lg-5"
-        >
-          <ul>
-            <div className="row gx-4 gx-lg-5 justify-content-center">
-              <div className="col-md-10 col-lg-8 col-xl-7">
-                <div className="post-preview">
-                  {BlogData.map((blog) => (
-                    <li key={blog.id}>
-                      <h2 className="post-title">
-                        <Link to={`/insights/${blog.id}`}>{blog.title}</Link>
-                      </h2>
-                      <h3 className="post-subtitle">
-                        <Link to={`/insights/${blog.id}`}>{blog.summary}</Link>
-                      </h3>
-                      <span className="post-meta">Posted on {blog.date}</span>
-                    </li>
-                  ))}
+      <div className="container px-4 px-lg-5">
+        <div className="row gx-4 gx-lg-5 justify-content-center">
+          <div className="col-md-10 col-lg-8 col-xl-7">
+            <div className="post-preview">
+              {BlogData.map((blog) => (
+                <div key={blog.id}>
+                  <h2 className="post-title">
+                    <Link to={`/insights/${blog.id}`} className="post-link">
+                      {blog.title}
+                    </Link>
+                  </h2>
+                  <h3 className="post-subtitle">
+                    <Link to={`/insights/${blog.id}`} className="post-link">
+                      {blog.summary}
+                    </Link>
+                  </h3>
+                  <span className="post-meta">Posted on {blog.date}</span>
                 </div>
-              </div>
+              ))}
             </div>
-          </ul>
+          </div>
         </div>
-      </header>
+      </div>
     </>
   );
 }
