f078380fdb137a7b5343383fab5d5dbd9bb04332
Emily <emily.zy.lin@gmail.com>
2024-08-27 20:29:53 -0500

    update homepage banner style

--- Frontend/src/layouts/HomePage/components/HomePageBanner.jsx
+++ Frontend/src/layouts/HomePage/components/HomePageBanner.jsx
@@ -17,7 +17,11 @@ function HomePageBanner() {
               Product Development | Full Stack Web Developer | Project
               Management
             </p>
-            <p>Understand the why, define the what, and execute the how.</p>
+            <p>
+              Understand the <b style={{ color: "#609c24" }}>WHY</b>, define the{" "}
+              <b style={{ color: "#609c24" }}>WHAT</b>, and execute the{" "}
+              <b style={{ color: "#609c24" }}>HOW</b>.
+            </p>
             <br></br>
             <p>
               <ReactTyped
@@ -25,13 +29,13 @@ function HomePageBanner() {
                   "With my background and skills, I know why products matter",
                 ]}
                 typeSpeed={50}
+                showCursor={false}
               />
               <br></br>
               <ReactTyped
-                strings={["and can help you to turn concepts into reality."]}
+                strings={["and can help you to turn fantasy into reality!"]}
                 typeSpeed={50}
                 startDelay={5000}
-                // loop
               />
             </p>
           </>
