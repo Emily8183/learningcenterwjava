bc8c3fbb79f237f8939d68000fcbbea612fc1284
Emily <emily.zy.lin@gmail.com>
2024-08-30 20:45:59 -0500

    update the banner of leetcode list

--- Frontend/src/layouts/Leetcode/components/LeetcodePageBanner.jsx
+++ Frontend/src/layouts/Leetcode/components/LeetcodePageBanner.jsx
@@ -1,14 +1,49 @@
 import React from "react";
 import PageBanner from "../../NavbarAndFooter/PageBanner";
+import { ReactTyped } from "react-typed";
 
 function LeetcodePageBanner() {
   return (
     <>
-      {/* TODO: update the content on the solution page */}
       <PageBanner
-        title="Leetcode Solutions"
-        content="
-          Write down leetcode everyday, my experience dadada"
+        title={
+          <>
+            <div>
+              <img
+                className="homepage-profile-pic"
+                src="../../../LC50days.png"
+                alt="Leetcode50"
+              />
+              <h1>LeetCode Solutions</h1>
+            </div>
+          </>
+        }
+        content={
+          <>
+            <p>
+              I started my LeetCode journey in June 2024 to sharpen my coding
+              skills.
+            </p>
+
+            <p>
+              Turns out there is way more fun than I thought, and I am learning
+              a ton along the way.
+            </p>
+            <p>
+              More features will be added to this page.{" "}
+              <b style={{ color: "#609c24" }}>Stay tuned!</b>
+            </p>
+            <br></br>
+            <p>
+              <ReactTyped
+                strings={[
+                  "A journey of a thousand miles begins with a single step.",
+                ]}
+                typeSpeed={50}
+              />
+            </p>
+          </>
+        }
       />
     </>
   );
