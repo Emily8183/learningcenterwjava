7e1cab2e16c2f8d4f3da7585296dcdc50ccb1b32
Emily <emily.zy.lin@gmail.com>
2024-09-05 11:07:56 -0500

    create insightArticlesRepo

---
+++ Backend/spring-boot-library/src/main/java/com/emproject/learningcenterwithjava/dao/InsightArticlesRepository.java
@@ -0,0 +1,7 @@
+package com.emproject.learningcenterwithjava.dao;
+
+import com.emproject.learningcenterwithjava.entity.InsightArticles;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InsightArticlesRepository extends JpaRepository<InsightArticles, Long> {
+}
