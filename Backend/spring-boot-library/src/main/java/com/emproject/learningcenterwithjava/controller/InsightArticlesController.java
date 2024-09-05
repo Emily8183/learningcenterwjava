13c01d097c0ec890cb567298085337449c63f9c3
Emily <emily.zy.lin@gmail.com>
2024-09-05 11:34:12 -0500

    create insightArticlesController

--- Backend/spring-boot-library/src/main/java/com/emproject/learningcenterwithjava/controller/InsightArticlesController.java
+++ Backend/spring-boot-library/src/main/java/com/emproject/learningcenterwithjava/controller/InsightArticlesController.java
@@ -1,9 +1,22 @@
 package com.emproject.learningcenterwithjava.controller;
 
 import com.emproject.learningcenterwithjava.dao.InsightArticlesRepository;
+import com.emproject.learningcenterwithjava.entity.InsightArticles;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("api/insightArticles")
@@ -11,4 +24,21 @@ public class InsightArticlesController {
 
     @Autowired
     private InsightArticlesRepository insightArticlesRepository;
+
+    @GetMapping("")
+    public ResponseEntity<CollectionModel<EntityModel<InsightArticles>>> getAllInsightArticles() {
+        List<EntityModel<InsightArticles>> insightArticles = insightArticlesRepository.findAll().stream()
+                .map(insightArticle -> EntityModel.of(insightArticle,
+                        linkTo(methodOn(InsightArticlesController.class).getInsightArticlesById(insightArticle.getId())).withSelfRel()))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(CollectionModel.of(insightArticles,
+                linkTo(methodOn(LeetcodeSolutionController.class)getAllInsightArticles()).withSelfRel()));
+    }
+
+    public ResponseEntity<InsightArticles> getInsightArticlesById(@PathVariable Long id) {
+        Optional<InsightArticles> insightArticles = insightArticlesRepository.findById(id);
+        return insightArticles.map(ResponseEntity::ok).orElseGet(()->ResponseEntity.notFound().build());
+    }
 }
+
