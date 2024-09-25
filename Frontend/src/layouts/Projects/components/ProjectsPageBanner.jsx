3880f41e308a7d1dbde338db0b2c5d79695e7189
Emily <emily.zy.lin@gmail.com>
2024-09-25 17:47:54 -0500

    update the banner of project journals

--- Frontend/src/layouts/Projects/components/ProjectsPageBanner.jsx
+++ Frontend/src/layouts/Projects/components/ProjectsPageBanner.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import PageBanner from "../../NavbarAndFooter/PageBanner";
+import { ReactTyped } from "react-typed";
 
 function ProjectsPageBanner() {
   return (
     <>
       {/* TODO: update the content on the projects page */}
       <PageBanner
-        title="Project Experiences"
-        content="
-          Write down diaries everyday, my experience dadada"
+        title="Project Journals"
+        content={
+          <>
+            <br></br>
+
+            <p>
+              不闻不若闻之，闻之不若见之，见之不若知之，知之不若行之。学至于行而止矣。行之，明也。
+            </p>
+            <p>
+              <ReactTyped
+                strings={[
+                  "Translation: I hear and I forget. I see and I remember. I do and I understand.    - Xun Zi",
+                ]}
+                typeSpeed={50}
+              />
+            </p>
+          </>
+        }
       />
     </>
   );
