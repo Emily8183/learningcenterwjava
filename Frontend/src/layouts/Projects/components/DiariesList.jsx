9916b78ad6f5cc5dbe20a260a711c37c3439a7a2
Emily <emily.zy.lin@gmail.com>
2024-09-25 21:23:37 -0500

    update project journals' list

--- Frontend/src/layouts/Projects/components/DiariesList.jsx
+++ Frontend/src/layouts/Projects/components/DiariesList.jsx
@@ -4,8 +4,13 @@ import axios from "axios";
 import { GoArrowUpRight } from "react-icons/go";
 // import DiariesData from "./DiariesData";
 
+function decodeHtmlEntities(str) {
+  return str.replace(/&amp;/g, "&");
+}
+
 function DiariesList() {
-  const [diaries, setDiaries] = useState([]);
+  const [diaries, setDiaries] = useState([]); //SQL
+  const [projectPosts, setProjectPosts] = useState([]); //Medium
 
   useEffect(() => {
     const fetchDiaries = async () => {
@@ -37,14 +42,49 @@ function DiariesList() {
     fetchDiaries();
   }, []);
 
+  useEffect(() => {
+    const fetchBlogPosts = async () => {
+      try {
+        const response = await fetch(
+          "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@emily.zy.lin"
+        );
+        const data = await response.json();
+        const items = data.items || [];
+
+        const updatedItems = items
+          // 提取 description 中的 img src
+          .map((post) => {
+            const coverImageMatch = post.description.match(
+              /<img[^>]+src="([^">]+)"/
+            );
+            const coverImage = coverImageMatch ? coverImageMatch[1] : null;
+
+            //decode &amp in title
+            const decodedTitle = decodeHtmlEntities(post.title);
+
+            return { ...post, coverImage, title: decodedTitle };
+          })
+          .filter((post) => {
+            const hasProjects = /insights/i.test(post.categories);
+            const isEnglishTitle = /english/i.test(post.categories);
+            return hasProjects && isEnglishTitle;
+          });
+
+        setProjectPosts(updatedItems);
+      } catch (error) {
+        console.error("Error fetching blog posts:", error);
+        // setBlogLoading(false);
+      }
+    };
+    fetchBlogPosts();
+  }, []);
+
   return (
     <>
       <section className="portfolio-section">
         <div className="container">
-          {/* <div className="container px-4 px-lg-5">
-        <div className="row gx-4 gx-lg-5 justify-content-center">
-          <div className="col-md-10 col-lg-8 col-xl-7"> */}
           <div className="portfolio-box">
+            {/* Blog Posts from my SQL*/}
             <div className="grid2">
               {diaries.map((projectDiary) => (
                 <div className="portfolio-item branding" key={projectDiary.id}>
@@ -76,6 +116,43 @@ function DiariesList() {
                   </div>
                 </div>
               ))}
+            </div>
+
+            <div>
+              {/* Blog Posts from Medium*/}
+              <div className="grid2">
+                {projectPosts.map((post, index) => (
+                  <div className="portfolio-item branding" key={index}>
+                    <div className="image-box">
+                      <img src={post.coverImage} alt="" />
+                    </div>
+
+                    <div className="content-box">
+                      <h3 className="portfolio-title">
+                        <Link
+                          to={post.link}
+                          style={{ textDecoration: "none" }}
+                          className="project-title"
+                        >
+                          {post.title}
+                        </Link>
+                      </h3>
+
+                      {/* <h4>
+                      <Link
+                        to={`/projectDiaries/${projectDiary.id}`}
+                        className="project-subtitle"
+                      >
+                        {projectDiary.summary}
+                      </Link>
+                    </h4> */}
+                      <i>
+                        <GoArrowUpRight size={50} />
+                      </i>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
