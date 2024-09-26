549248d68e6e1fd91f1a5a640442360fb6f57929
Emily <emily.zy.lin@gmail.com>
2024-09-26 14:33:16 -0500

    update project css

--- Frontend/src/layouts/Projects/components/ProjectsPageBanner.jsx
+++ Frontend/src/layouts/Projects/components/ProjectsPageBanner.jsx
@@ -12,13 +12,12 @@ function ProjectsPageBanner() {
           <>
             <br></br>
 
-            <p>
-              不闻不若闻之，闻之不若见之，见之不若知之，知之不若行之。学至于行而止矣。行之，明也。
-            </p>
+            <p>不闻不若闻之，闻之不若见之，见之不若知之，知之不若行之。</p>
+            <p>学至于行而止矣。行之，明也。</p>
             <p>
               <ReactTyped
                 strings={[
-                  "Translation: I hear, and I forget. I see, and I remember. I do, and I understand.   - Xun Tzu",
+                  "Translation: I hear, and I forget. I see, and I remember. I do, and I understand.",
                 ]}
                 typeSpeed={50}
               />
