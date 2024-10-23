81fc4179db73b2983724c432e6fb5bbdbec30bb6
Emily <emily.zy.lin@gmail.com>
2024-10-22 22:18:02 -0500

    update SolutionsData

--- Frontend/src/layouts/Leetcode/components/SolutionsData.jsx
+++ Frontend/src/layouts/Leetcode/components/SolutionsData.jsx
@@ -1,21 +1,21 @@
 const SolutionsData = [
   {
     id: 1,
-    title: "First solution Post",
-    summary: "This is the summary of the first solution post.",
-    category: "tips",
-    date: "Aug 3",
-    content:
-      "Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.",
+    title: "Merging and Searching Techniques for Binary Trees (LC617 & LC700)",
+    category: "Tree",
+    imageURL:
+      "https://miro.medium.com/v2/resize:fit:1400/format:webp/0*DJpZGgivlaaidoju",
+    link: "https://medium.com/@emily.zy.lin/leetcode-merging-and-searching-techniques-for-binary-trees-lc617-lc700-00cf45fa3990",
+    LeetcodeNum: ["617", "700"],
   },
   {
     id: 2,
-    title: "Second solution Post",
-    summary: "This is the summary of the second solution post.",
-    date: "Aug 4",
-    category: "tips",
-    content:
-      "Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.",
+    title: "Comparison Between LC78 Subsets & LC90 Subsets II",
+    category: "Backtracking",
+    imageURL:
+      "https://miro.medium.com/v2/resize:fit:942/format:webp/1*FcwyJsyc44t4DACjSF3r0A.png",
+    link: "https://medium.com/@emily.zy.lin/leetcode-comparison-between-lc78-subsets-lc90-subsets-ii-e89aca7999be",
+    LeetcodeNum: ["78", "90"],
   },
 ];
 
