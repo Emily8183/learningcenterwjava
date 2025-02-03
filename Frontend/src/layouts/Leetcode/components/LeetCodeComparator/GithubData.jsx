d8a4af3b12ab736ef2f6b3cf1f51fc5bf7b0b147
Emily <emily.zy.lin@gmail.com>
2025-02-02 21:00:50 -0600

    github fetching data -async function

--- Frontend/src/layouts/Leetcode/components/LeetCodeComparator/GithubData.jsx
+++ Frontend/src/layouts/Leetcode/components/LeetCodeComparator/GithubData.jsx
@@ -6,14 +6,28 @@ function GithubData() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const repoOwner = "Emily8183"; // 例如 "yourusername"
-    const repoName = "leetcode-study-notes"; // 例如 "leetcode-notes"
-    const filePath = "36_Valid_Sudoku.md"; // 例如 "two-sum.md"
+    const fetchMarkDown = async () => {
+      try {
+        const repoOwner = "Emily8183"; // 例如 "yourusername"
+        const repoName = "leetcode-study-notes"; // 例如 "leetcode-notes"
+        const filePath = "36_Valid_Sudoku.md"; // 例如 "two-sum.md"
 
-    const githubApiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`;
+        const githubApiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`;
 
-    fetch(githubApiUrl);
-  });
+        const response = await axios.get(githubApiUrl);
+
+        // 解码 Base64 内容, from markdown to html
+        const markdownContent = atob(response.data.content);
+        setMarkdown(markdownContent);
+        setError(null); //clear error
+      } catch (error) {
+        console.error("Error fetching Markdown file:", error);
+        setError("Failed to load Markdown content");
+      }
+    };
+
+    fetchMarkDown(); //调用这个异步函数
+  }, []);
 }
 
 export default GithubData;
