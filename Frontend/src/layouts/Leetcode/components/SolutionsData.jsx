4495a4e7dd920886b0b525f9d87bd0826a387915
Emily <emily.zy.lin@gmail.com>
2025-02-06 19:34:19 -0600

    new LC posts

--- Frontend/src/layouts/Leetcode/components/SolutionsData.jsx
+++ Frontend/src/layouts/Leetcode/components/SolutionsData.jsx
@@ -17,6 +17,23 @@ const SolutionsData = [
     link: "https://medium.com/@emily.zy.lin/leetcode-comparison-between-lc78-subsets-lc90-subsets-ii-e89aca7999be",
     LeetcodeNum: ["78", "90"],
   },
+  {
+    index: 3,
+    title: "LC116 & LC117: Next Right Pointers in Binary Tree",
+    category: "Tree",
+    imageURL:
+      "https://miro.medium.com/v2/resize:fit:640/format:webp/1*XXU_qfubrZ7RbkW9fmCZgw.png",
+    Link: "https://medium.com/@emily.zy.lin/leetcode-compare-the-implementation-of-next-right-pointers-in-binary-trees-lc116-lc117-ef9fd812db5b",
+    LeetcodeNum: ["116", "117"],
+  },
+  {
+    index: 4,
+    title: "Reversal Probs: Key Differences in Arrays vs. Linked Lists",
+    category: "comparison",
+    imageURL:
+      "https://miro.medium.com/v2/resize:fit:720/format:webp/1*EOtFpdMyKMXxjPGMkZYeQg.jpeg",
+    Link: "https://medium.com/@emily.zy.lin/arrays-vs-linked-lists-key-differences-and-the-related-leetcode-reversal-problems-1b5e1ae1520a",
+  },
 ];
 
 export default SolutionsData;
